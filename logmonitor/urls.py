526be24c748c2621a45be697591f197ef2240038
bergi <s.berke@netzkolchose.de>
2018-06-21 23:23:55 +0200

    fix urls

--- logmonitor/urls.py
+++ logmonitor/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^logs/', include('logview.urls')),
+    url(r'logs/', include('logview.urls')),
 ]
