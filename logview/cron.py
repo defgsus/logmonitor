64363896f1d51c9d32ec792a6c30c7ba98b4a1ba
bergi <s.berke@netzkolchose.de>
2018-06-22 15:25:59 +0200

    update cronjob times

--- logview/cron.py
+++ logview/cron.py
@@ -4,7 +4,7 @@ import kronos
 # see https://github.com/jgorset/django-kronos
 
 
-@kronos.register("0 2,6,10,14,18,22 * * *")
+@kronos.register("0 * * * *")
 def logview_update():
     """
     Read logs into database
