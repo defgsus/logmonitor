7407f6fb58f611db710e12863ce8636ff810781d
bergi <s.berke@netzkolchose.de>
2018-06-25 08:10:05 +0200

    add admin classes

--- logview/admin.py
+++ logview/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import LogFileEntry
+from .models import *
 
 
 class LogFileEntryAdmin(admin.ModelAdmin):
@@ -10,5 +10,34 @@ class LogFileEntryAdmin(admin.ModelAdmin):
         "__str__", "time", "date", "source_ip", "user", "task",
     )
 
-
 admin.site.register(LogFileEntry, LogFileEntryAdmin)
+
+
+class JobLogAdmin(admin.ModelAdmin):
+    exclude=[]
+    model=JobLog
+    list_display = (
+        "__str__", "name", "count", "date_started", "date_finished", "duration", "log_text", "error_text"
+    )
+
+admin.site.register(JobLog, JobLogAdmin)
+
+
+class WhoisRequestAdmin(admin.ModelAdmin):
+    exclude=[]
+    model=WhoisRequest
+    list_display = (
+        "__str__", "date", "ip", "response",
+    )
+
+admin.site.register(WhoisRequest, WhoisRequestAdmin)
+
+
+class NslookupRequestAdmin(admin.ModelAdmin):
+    exclude=[]
+    model=NslookupRequest
+    list_display = (
+        "__str__", "date", "ip", "response",
+    )
+
+admin.site.register(NslookupRequest, NslookupRequestAdmin)
